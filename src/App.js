8cda38d60216b0aaca135e884dc446eec340ef16
RonnieGarcia1 <86433181+RonnieGarcia1@users.noreply.github.com>
2021-11-19 13:07:07 -0500

    created initial layout & forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,105 @@
+import { Component } from "react";
 import "./App.css";
+import Form from "./components/Form";
+import data from "./data/productData";
+import formatPrice from "./helpers/formatPrice";
 
-const App = () => {
-  return <h1>Hello, world!</h1>;
-};
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      products: data,
+      name: "",
+      price: "",
+      subtotal: 0,
+      Tax: 0,
+      Total: 0,
+    }
+  }
 
+  addItemToCart = (e) => {
+    e.preventDefault();
+    this.setState({
+
+
+    })
+
+
+  }
+
+  firstName = (e) => {
+    e.preventDefault();
+    console.log("FirstName")
+  }
+
+  lastName = (e) => {
+    e.preventDefault();
+    console.log("LastName")
+  }
+
+  email = (e) => {
+    e.preventDefault();
+    console.log("Please enter a valid email")
+  }
+
+  creditCard = (e) => {
+    e.preventDefault();
+    console.log("Enter valid credit card")
+  }
+
+  zipCode = (e) => {
+    e.preventDefault();
+    console.log("enter a valid zip code")
+  }
+  
+  buyNow = (e) => {
+    e.preventDefault();
+    console.log("you have submitted your order")
+  }
+
+
+  render(){
+
+    let productList = this.state.products.map((list)=>{
+      let { id, name, price, description, img } = list;
+      console.log(id);
+      console.log(name);
+      console.log(price);
+      console.log(img);
+      console.log(description);
+      return(
+        <div className="products">
+          <div>
+            <h4>{ name }</h4>
+            <p>Price: { formatPrice(price) }</p>
+            <button onClick={()=>this.addItemToCart()}>Add To Cart</button>
+            <img src={img} alt="garage sale images" />
+            <p>{ description }</p>
+          </div>
+          <br />
+          <div></div>
+
+          
+
+        </div>
+      )
+    });
+
+
+    return(
+      <div className="app">
+      <body>
+        <h3>My Garage Sale</h3>
+
+        { productList }
+        <div id="form-section"> 
+        <Form />
+        </div>
+       </body>
+       </div>
+    )
+
+  }
+
+}
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,68 @@
+import { Component } from "react";
+
+class Form extends Component{
+    // constructor(){
+    //     super();
+    // }
+
+    render(){
+        return(
+            
+            <div className="cart">
+            <h3>Cart</h3>
+            <h4>Subtotal</h4>
+            <h4>Tax</h4>
+            <h4>Total</h4>
+
+            <h2>Checkout</h2>
+
+            <form id="checkout" onSubmit={this.props.buyNow}>
+            <label htmlFor="first-name">First Name</label><br />
+            <input 
+                onInput={this.props.firstName} 
+                value={this.props.firstName} 
+                type="text" 
+                name="first-name" 
+                id="first-name" />
+            <br />
+            <label htmlFor="last-name">Last Name</label><br />
+            <input 
+                onInput={this.props.lastName} 
+                value={this.props.lastName}
+                type="text" 
+                name="last-name" 
+                id="last-name" />
+            <br />
+            <label htmlFor="email">Email</label><br />
+            <input
+                onInput={this.props.email} 
+                value={this.props.email} 
+                type="text" 
+                name="email" 
+                id="email" />
+            <br />
+            <label htmlFor="credit-card">Credit Card</label><br /> 
+            <input
+                onInput={this.props.creditCard} 
+                value={this.props.creditCard}
+                type="number" 
+                name="credit-card" 
+                id="credit-card" />
+                <br />
+                <label htmlFor="zip-code">Zip Code</label><br /> 
+            <input
+                onInput={this.props.zipCode} 
+                value={this.props.zipCode}
+                type="number" 
+                name="zip-code" 
+                id="zip-code" />
+                <br />
+            <button type="submit">Buy Now</button>
+          </form>
+          </div>
+
+        )
+    }    
+}
+
+export default Form
